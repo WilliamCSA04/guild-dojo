aa22ea72b81fb04eb269f6127f94f60212be2be5
Williamcsa04 <williamcaetano14@gmail.com>
2020-02-09 18:40:58 -0300

    :sparkles: Added default theme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
-import GlobalStyles from "./styles/Global";
+import theme, { GlobalStyles } from "./styles";
 ReactDOM.render(
-  <>
+  <ThemeProvider theme={theme}>
     <GlobalStyles />
     <App />
-  </>,
+  </ThemeProvider>,
   document.getElementById("root")
 );
---
+++ src/styles/index.js
@@ -0,0 +1,12 @@
+import GlobalStyles from "./Global";
+
+export default {
+  colors: {
+    primary: "#ff47da",
+    secondary: "#ff87ab",
+    background: "#1b065e",
+    font: "#f5eccd"
+  }
+};
+
+export { GlobalStyles };
