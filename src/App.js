8d5a7671ef7ecbbe6469feec27bf3c177452e355
Williamcsa04 <williamcaetano14@gmail.com>
2020-02-09 18:28:43 -0300

    :construction: Added default form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Input from "./components/Input";
+import Button from "./components/Button";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <form>
+        <Input />
+        <Input />
+        <Input type="email" ariaLabel="email" />
+        <Input type="password" ariaLabel="password" />
+        <Input type="password" ariaLabel="confirm password" />
+        <Button type="submit" ariaLabel="enviar formulário">
+          Enviar
+        </Button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
