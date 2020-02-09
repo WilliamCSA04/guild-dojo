6ffcdb50cf985c88b859e4d225cf29d9d7b3c940
Williamcsa04 <williamcaetano14@gmail.com>
2020-02-09 17:45:33 -0300

    :art: Created a button story

---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Button from ".";
+
+export default { title: "Button" };
+
+export const Default = () => (
+  <Button ariaLabel="Default Story">Just a button</Button>
+);
