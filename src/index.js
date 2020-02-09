41ca90a5f265e0602c70c6d04ebfe4b270fa2f32
Williamcsa04 <williamcaetano14@gmail.com>
2020-02-09 17:03:43 -0300

    :sparkles: Added global styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import GlobalStyles from "./styles/Global";
+ReactDOM.render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
+  document.getElementById("root")
+);
