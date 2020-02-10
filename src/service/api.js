485926d5554767cac6ef006bba07018f944f90e6
Williamcsa04 <williamcaetano14@gmail.com>
2020-02-09 23:01:48 -0300

    :sparkles: Added age validation

--- src/service/api.js
+++ src/service/api.js
@@ -4,14 +4,21 @@ import mockAxios from "axios-mock-adapter";
 const mock = new mockAxios(axios);
 
 export function validateEmail(email) {
-  var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return email.match(regex);
 }
 
+export function validateAge(age) {
+  const isNumber = Number.isInteger(parseInt(age));
+  const minimumAge = 18;
+  return isNumber && age >= minimumAge;
+}
+
 export default function buildMock(params) {
-  const { email } = params;
+  const { email, age } = params;
   const invalidEmail = !validateEmail(email);
-  const hasErrors = invalidEmail;
+  const invalidAge = !validateAge(age);
+  const hasErrors = invalidEmail || invalidAge;
   if (hasErrors) {
     mock.onPost("/user", params).reply(418, {
       users: params
