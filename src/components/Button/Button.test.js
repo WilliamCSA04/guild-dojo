79f37f0ef38e49eb08c45fa598847ff2e5cc9333
Williamcsa04 <williamcaetano14@gmail.com>
2020-02-09 23:51:03 -0300

    :camera_flash: Added one more snapshot for button

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -3,9 +3,18 @@ import { render } from "@testing-library/react";
 import Button from ".";
 
 describe("<Button /> snapshot", () => {
-  it("renders the component without any changes", () => {
+  it("renders the component with default props", () => {
     const container = render(
       <Button ariaLabel="Testing">Testing Button</Button>
+    );
+    expect(container.baseElement).toMatchSnapshot();
+  });
+
+  it("renders the component with ariaLabelledby", () => {
+    const container = render(
+      <Button ariaLabel="Testing" ariaLabelledby="Testing">
+        Testing Button
+      </Button>
     );
     expect(container.baseElement).toMatchSnapshot();
   });
