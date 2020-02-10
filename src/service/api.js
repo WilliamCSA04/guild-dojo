5d3bd43d3e81531770aa806e34a3c32f1f4bc8ab
Williamcsa04 <williamcaetano14@gmail.com>
2020-02-09 22:55:58 -0300

    :clown_face: :egg: mock fail for invalid request

--- src/service/api.js
+++ src/service/api.js
@@ -2,8 +2,23 @@ import axios from "axios";
 import mockAxios from "axios-mock-adapter";
 
 const mock = new mockAxios(axios);
+
+export function validateEmail(email) {
+  var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return email.match(regex);
+}
+
 export default function buildMock(params) {
-  mock.onPost("/user", params).reply(200, {
-    users: params
-  });
+  const { email } = params;
+  const invalidEmail = !validateEmail(email);
+  const hasErrors = invalidEmail;
+  if (hasErrors) {
+    mock.onPost("/user", params).reply(418, {
+      users: params
+    });
+  } else {
+    mock.onPost("/user", params).reply(200, {
+      users: params
+    });
+  }
 }
