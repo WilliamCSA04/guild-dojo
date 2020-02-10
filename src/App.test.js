2f17e9c389a32e290369a78832ae29605f940d5b
Williamcsa04 <williamcaetano14@gmail.com>
2020-02-10 00:59:17 -0300

    :white_check_mark: Added simple test to check if all inputs exist

--- src/App.test.js
+++ src/App.test.js
@@ -5,12 +5,57 @@ import App from "./App";
 import theme from "./styles";
 
 describe("<App /> inputs", () => {
-  const container = render(
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  );
+  it("App must have one name input", () => {
+    const container = render(
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    );
+    const inputs = container.getAllByTestId("name");
+    expect(inputs).toHaveLength(1);
+  });
+  it("App must have one age input", () => {
+    const container = render(
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    );
+    const inputs = container.getAllByTestId("age");
+    expect(inputs).toHaveLength(1);
+  });
+  it("App must have one email input", () => {
+    const container = render(
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    );
+    const inputs = container.getAllByTestId("email");
+    expect(inputs).toHaveLength(1);
+  });
+  it("App must have one password input", () => {
+    const container = render(
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    );
+    const inputs = container.getAllByTestId("password");
+    expect(inputs).toHaveLength(1);
+  });
+  it("App must have one confirm password input", () => {
+    const container = render(
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    );
+    const inputs = container.getAllByTestId("confirmPassword");
+    expect(inputs).toHaveLength(1);
+  });
   it("Name input should change whenever user interact with it", () => {
+    const container = render(
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    );
     const input = container.getByTestId("name");
     const newValue = "Will Turner";
     fireEvent.change(input, { target: { defaultValue: newValue } });
